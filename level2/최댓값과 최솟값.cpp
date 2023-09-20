dca4476130c8783d013c19b799b0e29994d7710e
kocanory <kocan9803@naver.com>
2023-09-20 23:56:16 +0900

    최댓값과 최솟값
    - 문자열로 입력받아 띄어쓰기를 기준으로 토큰 분리하고 벡터에 저장
    - 오름차순으로 정렬
    - 최솟값과 최댓값 출력

---
+++ level2/최댓값과 최솟값.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = ""A;
+    vector<int>num;
+    stringstream ss(s);
+    string temp = "";
+    while(getline(ss, temp, ' '))
+    {
+        num.push_back(stoi(temp));
+    }
+    sort(num.begin(), num.end());
+    return answer = to_string(num.front()) + " " + to_string(num.back());
+}

113bc9642225cf3abcb8b628e8372b7ca0d843c6
kocanory <kocan9803@naver.com>
2024-01-15 02:52:25 +0900

    매칭 점수
    - 문자열 처리 활용
    - 이 문제에서 정규식 활용하면 코드 크기를 줄일 수 있음
    - url이나 word, 외부 링크를 찾는 방식은 해당 태그가 위치하는 인덱스 값을 얻어서 그 자리부터 태그 끝까지 문자열을 찾아서 저장하는 방식
    - 구조체를 정의하여 인덱스, basic point, 외부 링크, 링크 점수, 매치 점수 를 저장한다.
    - 대소문자 구분을 안하기 때문에 문자열을 전부 소문자나 대문자로 변환

---
+++ level3/매칭 점수.cpp
@@ -0,0 +1,128 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+typedef struct page{
+    int idx;
+    int basic_p;
+    vector<string> out_link;
+    double link_p;
+    double match_p;
+}page;
+
+string Word;
+map<string, int> page_num;
+vector<page> Pages;
+
+bool comp(page a, page b)
+{
+    if(a.match_p >= b.match_p)
+    {
+        if(a.match_p == b.match_p)
+        {
+            if(a.idx < b.idx)
+                return true;
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+string to_lower(string s)
+{
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
+
+string find_url(string s)
+{
+    string target = "<meta property=\"og:url\" content=\"https://";
+    int idx = s.find(target);
+    idx += target.length();
+    string url = "";
+    while(s[idx] != '\"') url += s[idx++];
+    return url;
+}
+
+int find_word(string s)
+{
+    string target1 = "<body>", target2 = "/<body>";
+    int idx1 = s.find(target1), idx2 = s.find(target2);
+    s = s.substr(idx1, idx2 - idx1);
+    
+    string cur = "";
+    int count = 0;
+    for(auto c : s)
+    {
+        if(isalpha(c) == 0){
+            if(cur == Word) count++;
+            cur = "";
+        }
+        else cur += c;
+    }
+    return count;
+}
+
+vector<string> find_outlink(string s)
+{
+    vector<string> result;
+    string target = "<a href=\"https://";
+    int idx = s.find(target);
+    while(idx != -1)
+    {
+        idx += target.length();
+        string cur = "";
+        while(s[idx] != '\"') cur += s[idx++];
+        result.push_back(cur);
+        s = s.substr(idx);
+        idx = s.find(target);
+    }
+    return result;
+}
+
+void calc_link()
+{
+    for(auto p : Pages)
+    {
+        vector<string> arr = p.out_link;
+        int size = arr.size();
+        for(auto a : arr)
+        {
+            if(page_num[a] == 0) continue;
+            int target_idx = page_num[a] - 1;
+            Pages[target_idx].link_p += ((double)p.basic_p / (double)size);
+        }
+    }
+    for(auto &p : Pages)
+        p.match_p = (double)p.basic_p + (double)p.link_p;
+    sort(Pages.begin(), Pages.end(), comp);
+}
+
+void func(vector<string> vec)
+{
+    for(int i = 0;i < vec.size();i++)
+    {
+        string s = to_lower(vec[i]);
+        string url = find_url(s);
+        page_num[url] = i + 1;
+        int bp = find_word(s);
+        vector<string> out_link = find_outlink(s);
+        Pages.push_back({i, bp, out_link, 0.0, 0.0});
+    }
+    calc_link();
+}
+
+int solution(string word, vector<string> pages) {
+    int answer = 0;
+    Word = to_lower(word);
+    func(pages);
+    for(auto p : Pages)
+        cout << p.idx << " " << p.basic_p << endl;
+    answer = Pages[0].idx;
+    return answer;
+}

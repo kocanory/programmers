58af0b7e2c7c1bf5a1bed04fc787e1632095ccf8
kocanory <kocan9803@naver.com>
2024-03-10 17:56:19 +0900

    도넛과 막대 그래프
    - edges를 순회하면서 도착 지점으로 향하지 않는 정점 중 연결된 간선의 개수가 큰 정점을 answer[0]에 저장한다.
    - 최정점에 연결된 정점을 벡터에 저장하고, 출발점이 겹치는 정점이 존재한다면 8자 모양 그래프이므로 answer[3]을 증가시킨다.
    - 최정점에 연결된 정점을 순회하면서, 다음 정점로 진행시키는데 만약 다음 정점이 처음 정점이라면 도넛 그래프이므로 answer[1]을 증가시킨다.
    - 8자, 도넛 그래프 외에는 막대 그래프이므로, starts 벡터의 크기에서 8자, 도넛 그래프의 개수를 뺀 값을 answer[2]에 저장한다.

---
+++ level2/도넛과 막대 그래프.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> edges) {
+    vector<int> answer(4);
+    unordered_map<int, int> root;
+    for(auto e : edges)
+        root[e[0]] += 1;
+    
+    for(auto e : edges)
+    {
+       if(root.find(e[1]) != root.end())
+           root.erase(e[1]);
+    }
+    
+    int center_count = -1;
+    for(auto r : root){
+        if(r.second > center_count){
+            answer[0] = r.first;
+            center_count = r.second;
+        }
+    }
+    
+    vector<int> starts;
+    unordered_map<int, int> node;
+    for(auto e : edges){
+        if(e[0] == answer[0]) starts.emplace_back(e[1]);
+        else{
+            if(node.find(e[0]) == node.end())
+                node[e[0]] = e[1];
+            else
+            {
+                node.erase(e[0]);
+                answer[3] += 1;
+            }
+        }
+    }
+    
+    for(auto s : starts){
+        if(node.find(s) == node.end()) continue;
+        
+        int next = node[s];
+        bool check = false;
+        
+        while(true){
+            if(node.find(next) != node.end()){
+                next = node[next];
+                if(s == next){
+                    check = true;
+                    break;
+                }
+            }
+            else break;
+        }
+        
+        if(check) answer[1] += 1;
+    }
+    answer[2] = starts.size() - answer[1] - answer[3];
+    return answer;
+}

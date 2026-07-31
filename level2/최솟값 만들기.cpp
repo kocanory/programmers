031cd7ebce26265b8037b887d9c915151156c0f8
kocan <kocan9803@naver.com>
2026-07-31 09:54:30 +0900

    최솟값 만들기
    - A는 오름차순, B는 내림차순으로 정렬한다. 그 후 각 원소끼리 곱한 후 더한다.
    - 모든 원소를 순회하면 더한 값을 반환한다.

--- level2/최솟값 만들기.cpp
+++ level2/최솟값 만들기.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int solution(vector<int> A, vector<int> B)
 {
     int answer = 0;
-
     sort(A.begin(), A.end());
-    sort(B.begin(), B.end(), greater<int>());
-    for(int i = 0;i<A.size();i++) answer += (A[i] * B[i]);
+    sort(B.begin(), B.end(), greater<>());
+    
+    for(int i = 0;i < A.size();i++)
+        answer += A[i] * B[i];
 
     return answer;
 }

bdd3866a045cf58e1391474f520df29a616db648
kocan <kocan9803@naver.com>
2026-08-01 10:52:17 +0900

    JadenCase 문자열 만들기
    - 문자열을 순회하면서 현재 문자가 공백인 경우 이전 문자를 공백으로 갱신한다. 만약 이전 문자가 공백이라면 첫 문자를 대문자로 바꾸고 문자열에 더한다. 그 외 문자는 소문자로 바꾸고 문자열에 더한다.
    - 모든 탐색이 끝나면 문자열을 반환한다.

--- level2/JadenCase 문자열 만들기.cpp
+++ level2/JadenCase 문자열 만들기.cpp
@@ -1,25 +1,23 @@
 #include <string>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
 string solution(string s) {
     string answer = "";
-    stringstream ss(s);
-    string temp = "";
-    bool flag = false;
-    while(getline(ss, temp, ' '))
-    {
-        if(flag) answer += " ";
-        for(int i = 0;i<temp.length();i++)
-        {
-            if(i == 0 && islower(temp[i])) temp[i] = toupper(temp[i]);
-            if(i != 0 && isupper(temp[i])) temp[i] = tolower(temp[i]);
+    char start = ' ';
+    
+    for(auto a : s){
+        if(a == ' ') start = a;
+        else if(start == ' '){
+            a = toupper(a);
+            start = a;
         }
-        if(!flag) flag = true;
-        answer += temp;
+        else
+            a = tolower(a);
+        
+        answer += a;
     }
-    if(s[s.length() - 1] == ' ') answer += " ";
+    
     return answer;
 }

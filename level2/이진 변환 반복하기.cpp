4be16dd4686d347df24c936425ff638475bea330
kocan <kocan9803@naver.com>
2026-08-02 12:57:14 +0900

    이진 변환 반복하기
    - 현재 문자열에서 0의 개수와 1의 개수를 구한다. 그 후 0의 개수를 삭제한 개수에 더하고, 1의 개수를 2진수로 변환한다. 그 후 연산 횟수를 증가시킨다.
    - 문자열의 길이가 1이 되면 카운트와 삭제 개수를 answer 벡터에 저장한 후 반환한다.

--- level2/이진 변환 반복하기.cpp
+++ level2/이진 변환 반복하기.cpp
@@ -5,30 +5,24 @@ using namespace std;
 
 vector<int> solution(string s) {
     vector<int> answer;
-    int count = 0, erased = 0;
-    for(int i = 0;i<s.length();i++)
-    {
-        while(s[i] == '0'){
-            erased++;
-            s.erase(i, 1);
+    int cnt = 0, deleted = 0, one = 0;
+    
+    while(s.size() != 1){
+        one = 0;
+        for(auto a : s){
+            if(a == '1') one++;
+            else deleted++;
         }
+        
+        s = "";
+        while(one){
+            s = to_string(one % 2) + s;
+            one /= 2;
+        }
+        cnt++;
     }
     
-    while(s != "1")
-    {        
-        int size = s.length();
-        s.clear();
-        while(size != 0)
-        {
-            if(size % 2)
-                s = to_string(size % 2) + s;
-            else
-                erased++;
-            size /= 2;
-        }
-        count++;
-    }
-    answer.push_back(count + 1);
-    answer.push_back(erased);
+    answer.push_back(cnt);
+    answer.push_back(deleted);
     return answer;
 }

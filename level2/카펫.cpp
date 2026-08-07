0f980d2a2dae070b5d070c04811c2b709e7a5e6c
kocan <kocan9803@naver.com>
2026-08-07 11:01:17 +0900

    카펫
    - 중앙에 있는 노란색 범위의 길이를 탐색하면서 만약 노란색 범위 양 옆까지 포함한 넓이가 전체 넓이를 만족하는 경우 (해당 길이 + 2)와 (다른 길이 + 2)를 벡터에 저장하고 탐색을 종료한다.
    - 탐색이 끝나면 길이가 저장된 벡터를  반환한다.

--- level2/카펫.cpp
+++ level2/카펫.cpp
@@ -6,20 +6,14 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    for(int i = 1;i<=sqrt(yellow);i++)
-    {
-        if(yellow % i == 0)
-        {
-            int size = (yellow / i + 2) * (i + 2);
-            if(size - yellow == brown)
-            {
-                answer.push_back(yellow / i + 2);
-                answer.push_back(i + 2);
-                break;
-            }
-        }
-        
-    }
-    return answer;
     
+    for(int i = 1;i <= sqrt(yellow);i++){
+        if(yellow % i == 0 && (i + 2) * (yellow / i + 2) == (brown + yellow)){
+            answer.push_back(yellow / i + 2);
+            answer.push_back(i + 2);
+            break;
+        }
+    }
+    
+    return answer;
 }

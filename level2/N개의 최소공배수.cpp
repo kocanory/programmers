85f47e2d2ef9d6dc6b45423bb2a1617009b03dfa
kocan <kocan9803@naver.com>
2026-08-12 11:49:30 +0900

    N개의 최소공배수
    - 처음 수를 최소공배수로 지정한다. 그 후 두 번째 수부터 순회하면서 현재 최소공배수와 현재 수의 최소공배수를 구한 뒤 갱신한다.
    - 최소 공배수를 구할 때 두 수의 곱에 두 수의 최대공약수를 나눈 값을 활용한다.
    - 모든 탐색이 끝나면 최소공배수를 반환한다.

--- level2/N개의 최소공배수.cpp
+++ level2/N개의 최소공배수.cpp
@@ -1,43 +1,16 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-int gcd(int a, int b)
-{
-    if(a < b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-    while(b > 0)
-    {
-        int n = a % b;
-        a = b;
-        b = n;
-    }
-    return a;
-}
-
-int lcm(int a, int b)
-{
+int lcm(int a, int b){
     return a * b / gcd(a, b);
 }
 
 int solution(vector<int> arr) {
-    int answer = 0;
-    for(int i = 0;i<arr.size();i++)
-    {
-        if(answer == 0)
-        {
-            answer = lcm(arr[i], arr[i + 1]);
-            i++;
-        }
-        else
-        {
-            answer = lcm(answer, arr[i]);
-        }
-    }
+    int answer = arr[0];
+    for(int i = 1;i < arr.size();i++)
+        answer = lcm(answer, arr[i]);
     return answer;
 }

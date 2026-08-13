49821f807f637336665ef137a794d0b59dcb959f
kocan <kocan9803@naver.com>
2026-08-13 14:33:36 +0900

    연속 부분 수열 합의 개수
    - 자리를 순회하면서 가능한 연속 부분 수열의 합을 구하고 set 자료형에 저장한다.
    - 모든 순회가 끝나면 set 자료형에 저장된 원소의 개수를 반환한다.

--- level2/연속 부분 수열 합의 개수.cpp
+++ level2/연속 부분 수열 합의 개수.cpp
@@ -1,23 +1,19 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <set>
 
 using namespace std;
 
 int solution(vector<int> elements) {
-    int answer = 0;
-    vector<int> arr;
-    for(int i = 1;i<=elements.size();i++)
-    {
-        for(int j = 0;j<elements.size();j++)
-        {
-            int sum = 0;
-            for(int k = 0;k < i;k++)
-                sum += elements[(j + k) % elements.size()];
-            arr.push_back(sum);
+    set<int> s;
+    
+    for(int i = 0, sum;i < elements.size();i++){
+        sum = 0;
+        for(int j = 0;j < elements.size();j++){
+            sum += elements[(i + j) % elements.size()];
+            s.insert(sum);
         }
     }
-    sort(arr.begin(), arr.end());
-    arr.erase(unique(arr.begin(), arr.end()), arr.end());
-    return answer = arr.size();
+    
+    return s.size();
 }

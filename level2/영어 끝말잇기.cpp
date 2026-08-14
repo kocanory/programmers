3111435041125c75e50f05e956ee8bd78c7b413d
kocan <kocan9803@naver.com>
2026-08-14 13:53:49 +0900

    영어 끝말잇기
    - 단어의 길이가 1이거나, 이전 단어의 끝과 현재 단어의 처음이 다르거나, 이전에 나왔던 단어라면 현재 순번과 차례를 answer 벡터에 저장하고 탐색을 종료한다. 그렇지 않으면 이전 단어와 단어 방문 여부를 갱신한다.
    - 탐색이 끝나면 answer 벡터를 반환한다.

--- level2/영어 끝말잇기.cpp
+++ level2/영어 끝말잇기.cpp
@@ -1,29 +1,25 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    vector<int> answer;
+    vector<int> answer = {0, 0};
+    map<string, bool> check;
     
-    vector<string> spoke;
-    for(int i = 0;i<words.size();i++)
-    {
-        if(find(spoke.begin(), spoke.end(), words[i]) == spoke.end() && ((i > 0 && words[i][0] == words[i - 1][words[i-1].length() - 1]) || i == 0))
-            spoke.push_back(words[i]);
-        else
-        {
-            answer.push_back(i % n + 1);
-            answer.push_back(i / n + 1);
+    string back = words[0];
+    check[back] = true;
+    
+    for(int i = 1;i < words.size();i++){
+        if(words[i].size() == 1 || back[back.size() - 1] != words[i][0] || check[words[i]]){
+            answer[0] = i % n + 1;
+            answer[1] = i / n + 1;
             break;
         }
+        back = words[i];
+        check[back] = true;
     }
-    if(spoke.size() == words.size())
-    {
-        answer.push_back(0);
-        answer.push_back(0);
-    }
+
     return answer;
 }

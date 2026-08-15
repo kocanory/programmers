19abc37c96f6b226ae0726aa3c07fcbf35028e36
kocan <kocan9803@naver.com>
2026-08-15 13:18:29 +0900

    예상 대진표
    - 라운드 횟수를 증가시킨다. 그 후 만약 a와 b의 다음 라운드 번호가 같다면 같은 그룹에 있는 것이므로 탐색을 종료한다. 그렇지 않으면 a와 b를 2로 나눈 값을 올림한 후 갱신한다.
    - 탐색이 종료되면 라운드 횟수를 반환한다.

--- level2/예상 대진표.cpp
+++ level2/예상 대진표.cpp
@@ -1,26 +1,16 @@
-#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int solution(int n, int a, int b)
 {
     int answer = 0;
-    if(a > b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-    
-    while(true)
-    {
+
+    while(true){
         answer++;
-        if(a == (b - 1) && a % 2) break;
-        if(a % 2) a = (a + 1) / 2;
-        else a /= 2;
-        
-        if(b % 2) b = (b + 1) / 2;
-        else b /= 2;   
+        if(ceil(a / (double)2) == ceil(b / (double)2)) break;
+        a = ceil(a / (double)2);
+        b = ceil(b / (double)2);
     }
 
     return answer;

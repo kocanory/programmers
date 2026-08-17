c44d2b126320605f2e778aa72d00d0278851b34d
kocan <kocan9803@naver.com>
2026-08-17 12:23:44 +0900

    괄호 회전하기
    - 괄호를 저장할 벡터가 비어있거나 현재 문자가 열린 괄호라면 벡터에 저장하다. 그렇지 않다면 벡터 마지막에 저장된 괄호가 열린 괄호인지 확인하고, 열린 괄호라면 현재 닫힌 괄호와 맞는 쌍인지 확인한다. 맞다면 벡터 마지막 값을 제거한다.
    - 모든 괄호를 탐색했다면 벡터가 비어있다면 확인한다. 비어있다면 괄호를 모두 만족하는 것이므로 카운트를 증가시킨다. 그 후 처음 문자를 문자 맨 뒤에 이동시킨다. 그 후 괄호 탐색을 진행한다.
    - 모든 탐색이 끝나면 카운트를 반환한다.

--- level2/괄호 회전하기.cpp
+++ level2/괄호 회전하기.cpp
@@ -1,51 +1,32 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
-    int size = s.length();
-    for(int i = 0;i<size;i++)
-    {
-        vector<char> temp;
-        for(auto a : s)
-        {
-            if(a == '(' || a == '[' || a == '{')
-                temp.push_back(a);
-            
-            if(temp.empty() && (a == ')' || a == ']' || a == '}'))
-            {
-                temp.push_back(a);
-                break;
-            }
-            
-            if(a == ')' && temp.back() == '(')
-                temp.pop_back();
-            else if(a == ')' && temp.back() != '(')
-            {
-                break;
-            }
-            
-            if(a == ']' && temp.back() == '[')
-                temp.pop_back();
-            else if(a == ']' && temp.back() != '[')
-            {
-                break;
-            }
-            
-            if(a == '}' && temp.back() == '{')
-                temp.pop_back();
-            else if(a == '}' && temp.back() == '{')
-            {
-                break;
+    
+    map<char, char> check = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+    
+    for(int i = 0;i < s.size();i++){
+        vector<char> arr;
+        
+        for(auto a : s){
+            if(arr.empty() || a == '(' || a == '{' || a == '[')
+                arr.push_back(a);
+            else{
+                if(check[arr.back()] == ' ') continue;
+                if(a == check[arr.back()])
+                    arr.pop_back();
             }
         }
         
-        if(temp.empty()) answer++;
+        if(arr.empty()) answer++;
         
         s += s[0];
         s.erase(0, 1);
     }
+    
     return answer;
 }

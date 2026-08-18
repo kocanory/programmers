0ed34464acd548d3b530c16851ef7b668c80e446
kocan <kocan9803@naver.com>
2026-08-18 12:09:34 +0900

    n^2 배열 자르기
    - left와 right 사이를 순회하면서 행과 열 번호를 구한다. 그 후 행이 얼마나 떨어져 있는지 구하고, 열과 행 번호를 비교해 얼마나 떨어져 있는지 확인한다. 이 때 열 번호가 행 번호보다 큰 경우에만 차를 구하고, 그렇지 않으면 0을 더한다. 그 후 다 더한 후 벡터에 저장한다.
    - 모든 탐색이 끝나면 벡트를 반환한다.

--- level2/n^2 배열 자르기.cpp
+++ level2/n^2 배열 자르기.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
-    for(long long i = left;i<=right;i++)
-    {
-        long long value = max(i / n, i % n) + 1;
-        answer.push_back(value);
-    }
+    
+    for(long long i = left;i <= right;i++)
+        answer.push_back(i / n + 1 + max(0ll, i % n - i / n));
+    
     return answer;
 }

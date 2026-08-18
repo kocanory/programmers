ba432ddb5a840c95cbdf69d52f9ed16f067f4ab3
kocan <kocan9803@naver.com>
2026-08-19 00:25:00 +0900

    의상
    - 의상 종류에 따른 갯수를 확인한다. 그 후 종류별 나올 수 있는 경우의 수(안 입는 경우 + 해당 부위의 옷 개수)를 곱해나간다.
    - 경우의 수 -1을 반환한다. (모두 안 입는 경우의 수를 제거하기 위해)

--- level2/의상.cpp
+++ level2/의상.cpp
@@ -1,17 +1,18 @@
 #include <string>
 #include <vector>
 #include <map>
-using namespace std;
 
-int cloth[4] = {0, };
+using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    map<string, int> m;
-    for(auto a : clothes)
-        m[a[1]]++;
-    for(auto a : m)
-        answer *= (a.second + 1);
-    answer -= 1;
-    return answer;
+    map<string, int> check;
+    
+    for(auto c : clothes)
+        check[c[1]]++;
+    
+    for(auto [k, v] : check)
+        answer *= (v + 1);
+    
+    return answer - 1;
 }

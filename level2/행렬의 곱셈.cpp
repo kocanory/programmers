b9b1c901fcfd42420e16f97c57948cc3a46f1ae3
kocan <kocan9803@naver.com>
2026-08-20 09:50:29 +0900

    행렬의 곱셈
    - 첫 번째 행렬의 행과 두 번째 행렬의 열을 순회하면서 대응되는 값을 곱하고 더한다. 그 후 더한 값을 벡터에 저장한 다음 한 행의 탐색이 끝나면 벡터를 answer 벡터에 저장한다.
    - 모든 탐색이 끝나면 answer 벡터를 반환한다.

--- level2/행렬의 곱셈.cpp
+++ level2/행렬의 곱셈.cpp
@@ -5,19 +5,17 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer;
-    for(int i = 0;i<arr1.size();i++)
-    {
-        vector<int> temp;
-        for(int j = 0;j<arr2[0].size();j++)
-        {
-            int value = 0;
-            for(int k = 0;k<arr1[0].size();k++)
-            {
-                value += (arr1[i][k] * arr2[k][j]); 
-            }
-            temp.push_back(value);
+    
+    for(int i = 0;i < arr1.size();i++){
+        vector<int> arr;
+        for(int j = 0, sum;j < arr2[0].size();j++){
+            sum = 0;
+            for(int k = 0;k < arr1[0].size();k++)
+                sum += arr1[i][k] * arr2[k][j];
+            arr.push_back(sum);
         }
-        answer.push_back(temp);
+        answer.push_back(arr);
     }
+    
     return answer;
 }

1a8fe50f0a1a9f063a10b9f33d8802a90a9c0133
kocan <kocan9803@naver.com>
2026-08-23 00:56:49 +0900

    피로도
    - 재귀함수를 통해 던전을 탐색하는 모든 조합을 찾는다. 첫 던전부터 순회하면서 만약 방문하지 않았고, 현재 남은 피로도가 해당 던전의 최소 피로도보다 크거나 같다면 방문 여부를 true로 바꾼 후 해당 던전을 탐색한다. 그 후 해당 던전의 방문 여부를 false로 바꾸고 다음 던전을 탐색한다.
    - 던전을 탐색할 때마다 탐색한 던전 수를 갱신한다.
    - 모든 탐색이 끝나면 탐색한 던전 수 answer를 반환한다.

--- level2/피로도.cpp
+++ level2/피로도.cpp
@@ -3,33 +3,21 @@
 
 using namespace std;
 
-void dfs(int &answer, int k, vector<vector<int>> dungeons, vector<bool> visited)
-{
-    int count = 0;
-    for(auto a : visited)
-        if(a) count++;
-    if(answer < count)
-        answer = count;
+int answer = -1;
+
+void dfs(int cnt, int remains, vector<vector<int>> dungeons, vector<bool> visited){
+    answer = max(answer, cnt);
     
-    for(int i = 0;i<dungeons.size();i++)
-    {
-        if(!visited[i])
-        {
-            if(k >= dungeons[i][0])
-            {
-                visited[i] = true;
-                k -= dungeons[i][1];
-                dfs(answer, k, dungeons, visited);
-                k += dungeons[i][1];
-                visited[i] = false;
-            }
+    for(int i = 0;i < dungeons.size();i++){
+        if(!visited[i] && remains >= dungeons[i][0]){
+            visited[i] = true;
+            dfs(cnt + 1, remains - dungeons[i][1], dungeons, visited);
+            visited[i] = false;
         }
     }
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = -1;
-    vector<bool> visited(dungeons.size(), false);
-    dfs(answer, k, dungeons, visited);
+    dfs(0, k, dungeons, vector<bool>(dungeons.size()));
     return answer;
 }

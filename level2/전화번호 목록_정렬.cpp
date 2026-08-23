cfb5914b4cedb929c5dd5ecbf8f6a1d20e49e6eb
kocan <kocan9803@naver.com>
2026-08-24 01:07:27 +0900

    전화번호 목록_정렬
    - 전화번호를 사전 순으로 정렬한다. 그 후 앞에 있는 전화번호가 뒤에 있는 전화번호의 접두사에 있는지 확인한다. 접두사에 있다면 false를 반환한다. 모든 탐색이 끝난다면 true를 반환한다.
    - 접두사인지 확인할 때에는 뒤에 있는 전화번호를 앞에 있는 전화번호의 길이만큼 잘라내 앞에 있는 전화번호와 같은지 확인한다.

--- level2/전화번호 목록_정렬.cpp
+++ level2/전화번호 목록_정렬.cpp
@@ -1,16 +1,15 @@
-#include <string>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
     sort(phone_book.begin(), phone_book.end());
-    for(int i = 0;i<phone_book.size() - 1;i++)
-    {
-        if(phone_book[i + 1].find(phone_book[i]) == 0)
+    
+    for(int i = 0;i < phone_book.size() - 1;i++)
+        if(phone_book[i + 1].substr(0, phone_book[i].size()) == phone_book[i])
             return false;
-    }
-    return answer;
+    
+    return true;
 }

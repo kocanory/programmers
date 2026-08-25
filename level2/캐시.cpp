8364ef5b89235f225c0858f8ded9fbaaee1a427e
kocan <kocan9803@naver.com>
2026-08-26 01:10:18 +0900

    캐시
    - 각 도시 이름을 순회하면서 소문자로 변환한다.
    - 이전에 저장한 도시 정보를 순회해 현재 도시 정보가 있는지 확인한다. 만약 있다면, 시간을 갱신하고 시간 순으로 재정렬한다. 그 후 처리시간을 1 증가시킨다.
    - 만약 이전에 저장한 도시 정보에 현재 도시가 없다면 1) 저장한 도시 정보의 개수가 cacheSize보다 작다면 현재 도시 정보와 시간을 저장하고 2) 저장한 도시 정보의 개수가 cacheSize보다 크다면 가장 오래된(맨 앞에 있는) 도시 정보를 삭제하고, 현재 도시 정보와 시간을 저장한다. (이때, cacheSize가 0이라면 도시 정보를 저장하지 못하도록 한다.) 그 후 처리시간을 5 증가시킨다.
    - 모든 탐색이 끝나면 처리 시간 answer를 반환한다.

--- level2/캐시.cpp
+++ level2/캐시.cpp
@@ -4,46 +4,37 @@
 
 using namespace std;
 
-bool compare(pair<string, int> a, pair<string, int> b)
-{
-    return a.second < b.second;
-}
-
 int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
-    vector<pair<string, int>> cache;
-    for(auto a : cities)
-    {
-        for(int i = 0;i<a.length();i++)
-                a[i] = tolower(a[i]);
-
-        if(cacheSize > 0)
-        {
-            int index = -1;
-            for(int i = 0;i<cache.size();i++)
-            {
-                if(cache[i].first == a)
-                {
-                    index = i;
-                    cache[i].second = 0;
-                    answer++;
-                }
-                else
-                    cache[i].second++;
-            }
-
-            if(index == -1)
-            {
-                if(!cache.empty() && cache.size() >= cacheSize)
-                {
-                    sort(cache.begin(), cache.end(), compare);
-                    cache.pop_back();
-                }
-                cache.push_back(make_pair(a, 0));
-                answer += 5;
+    vector<pair<int, string>> check;
+    
+    for(int i = 0;i < cities.size();i++){
+        for(auto &c : cities[i])
+            c = tolower(c);
+        
+        bool flag = false;
+        for(int idx = 0;idx < check.size();idx++){
+            if(check[idx].second == cities[i]){
+                check[idx].first = i;
+                sort(check.begin(), check.end());
+                flag = true;
+                answer++;
+                break;
             }
         }
-        else answer+=5;
+        
+        if(!flag){
+            if(check.size() < cacheSize)
+                check.push_back({i, cities[i]});
+            else{
+                if(cacheSize != 0){
+                    check.erase(check.begin(), check.begin() + 1);
+                    check.push_back({i, cities[i]});
+                }
+            }
+            answer += 5;
+        }
     }
+    
     return answer;
 }

7992e827263a9aafe72a6fb136168ad620432c6f
kocan <kocan9803@naver.com>
2026-08-27 00:49:31 +0900

    게임 맵 최단거리
    - bfs
    - (0, 0) 좌표를 큐에 넣고, 큐에서 좌표를 꺼내 4 방향을 탐색하면서 범위 안에 있고, 방문하지 않았으며, 벽이 없는 곳이면 해당 좌표를 큐에 넣고 탐색을 진행한다.
    - 만약 큐에서 꺼낸 현재 좌표가 (n - 1, m - 1)이라면 카운트를 반환하고 탐색을 종료한다. (bfs는 가장 짧은 거리임을 보장한다.)
    - 큐에 저장된 모든 좌표를 탐색했음에도 도착하지 못했다면 -1을 반환한다.

--- level2/게임 맵 최단거리.cpp
+++ level2/게임 맵 최단거리.cpp
@@ -1,68 +1,36 @@
-#include<vector>
-#include<queue>
+#include <vector>
+#include <queue>
+#include <tuple>
+
 using namespace std;
 
-void bfs(vector<vector<int>> &array, vector<vector<int>>maps)
+vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+queue<tuple<int, int, int>> q;
+vector<vector<bool>> check;
+
+int solution(vector<vector<int>> maps)
 {
-    int dx[] = {1, 0, -1, 0};
-    int dy[] = {0, 1, 0, -1};
-    int start_x = 0, start_y = 0;
-    array[start_x][start_y] = 1;
-    queue<pair<int, int>> q;
-    q.push(make_pair(start_x, start_y));
-    vector<vector<bool>> visit;
-    for(int i = 0;i<maps.size();i++)
-    {
-        vector<bool> temp(maps[0].size(), false);
-        visit.push_back(temp);
-    }
-    while(!q.empty())
-    {
-        pair<int, int> now = q.front();
+    int answer = -1;
+    int n = maps.size(), m = maps[0].size();
+    check.assign(n, vector(m, false));
+    
+    q.push({0, 0, 1});
+    check[0][0] = true;
+    
+    while(!q.empty()){
+        auto [x, y, cnt] = q.front();
         q.pop();
-        for(int i = 0;i<4;i++)
-        {
-            pair<int, int> next = now;
-            next.first += dx[i];
-            next.second += dy[i];
-            if(next.first >= 0 && next.first < maps.size() && next.second >= 0 && next.second < maps[0].size())
-            {
-                if(maps[next.first][next.second])
-                {
-                    if(!visit[next.first][next.second])
-                    {
-                        if(!(next.first == maps.size() - 1 && next.second == maps[0].size() - 1))
-                        {
-                            array[next.first][next.second] = array[now.first][now.second] + 1;
-                    
-                            visit[next.first][next.second] = true;
-                            q.push(next);
-                        }
-                        else
-                        {
-                            array[next.first][next.second] = array[now.first][now.second] + 1;
-                    
-                            visit[next.first][next.second] = true;
-                            queue<pair<int, int>> reset;
-                            q = reset;
-                        }
-                    }
-                }
-                
-            }            
+        
+        if(x == n - 1 && y == m - 1)
+            return cnt;
+        
+        for(auto [dx, dy] : dir){
+            int nx = x + dx, ny = y + dy;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m || check[nx][ny] || !maps[nx][ny]) continue;
+            
+            q.push({nx, ny, cnt + 1});
+            check[nx][ny] = true;
         }
     }
-}
-
-int solution(vector<vector<int> > maps)
-{
-    int answer = 0;
-    vector<vector<int>> array;
-    for(int i = 0;i<maps.size();i++)
-    {
-        vector<int> temp(maps[0].size(), -1);
-        array.push_back(temp);
-    }
-    bfs(array, maps);
-    return answer = (array[maps.size() - 1][maps[0].size() - 1] != -1) ? array[maps.size() - 1][maps[0].size() - 1] : -1;
+    return answer;
 }

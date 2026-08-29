c4f6e0aec57cd852f0d07f5ef8e970def22ac060
kocan <kocan9803@naver.com>
2026-08-29 13:40:46 +0900

    튜플
    - 튜플 내 존재하는 수의 집합을 별도의 벡터에 저장한다. 그 후 집합의 크기를 기준으로 오름차순으로 정렬한다. 집합을 순회하면서 집합 내 수가 answer에 존재하는지 확인하고, 존재하지 않는다면 수를 answer에 저장한다.
    - 모든 탐색이 끝나면 answer 벡터를 반환한다.

--- level2/튜플.cpp
+++ level2/튜플.cpp
@@ -1,41 +1,43 @@
 #include <string>
 #include <vector>
-#include <sstream>
-
-using namespace std;
 #include <algorithm>
 
-bool compare(string a, string b)
-{
-    return a.length() < b.length();
+using namespace std;
+
+bool cmp(vector<int> a, vector<int> b){
+    return a.size() < b.size();
 }
 
 vector<int> solution(string s) {
-    vector<int> answer;
-    vector<string> arr;
-    string str = "";
-    for(int i = 1;i<s.length() - 1;i++)
-    {
-        if(s[i] == '{') continue;
-        else if(s[i] == '}') 
-        {
-            arr.push_back(str);
-            str.clear();
-            i++;
+    vector<int> answer, nums;
+    vector<vector<int>> arr;
+    string num = "";
+    
+    for(int i = 1;i < s.size() - 1;i++){
+        if(s[i] == '{'){
+            num.clear();
+            nums.clear();
         }
-        else str += s[i];
+        else if(s[i] == '}'){
+            nums.push_back(stoi(num));
+            arr.push_back(nums);
+        }
+        else{
+            if(s[i] == ','){
+                nums.push_back(stoi(num));
+                num.clear();
+            }
+            else num += s[i];
+        }
     }
     
-    sort(arr.begin(), arr.end(), compare);
+    sort(arr.begin(), arr.end(), cmp);
     
-    for(auto a : arr)
-    {
-        stringstream ss(a);
-        string temp = "";
-        while(getline(ss, temp, ','))
-            if(find(answer.begin(), answer.end(), stoi(temp)) == answer.end())
-                answer.push_back(stoi(temp));
-        
+    for(auto a : arr){
+        for(auto n : a)
+            if(find(answer.begin(), answer.end(), n) == answer.end())
+                answer.push_back(n);
     }
+    
     return answer;
 }

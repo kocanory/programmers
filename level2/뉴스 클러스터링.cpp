164205e31b06c14ce1f881e3fb987f1a91536903
kocanory <kocan9803@naver.com>
2026-09-03 22:19:39 +0900

    뉴스 클러스터링
    - 문자열 str1, str2 모두 소문자로 변환한다.
    - 각 문자열을 순회하면서 문자 2개씩 자른다. 그 후 문자들이 모두 영문자인지 확인한 후 영문자라면 map 자료형에 저장하고, 원소 개수를 카운트한다.
    - 단어가 저장된 map을 순회하면서 중복되는 문자의 개수를 구한다. 해당 단어의 개수와 다른 map에 저장된 해당 단어의 개수를 비교해 작은 개수를 교집합 카운트에 증가시킨다.
    - 만약 두 map 모두 비어있다면 1에 65536을 곱한 후 반환하고, 그렇지 않으면 교집합 / 합집합 * 65536 을 반환한다.
    - 합집합의 경우 두 단어의 개수 합에서 교집합을 빼서 구했다.

--- level2/뉴스 클러스터링.cpp
+++ level2/뉴스 클러스터링.cpp
@@ -1,52 +1,33 @@
 #include <string>
-#include <vector>
+#include <map>
 
 using namespace std;
 
 int solution(string str1, string str2) {
-    int answer = 0;
-    for(auto &a : str1)
-        a = tolower(a);
-    for(auto &a : str2)
-        a = tolower(a);
+    double answer = 0, s1 = 0, s2 = 0, cnt = 0;
+    map<string, int> m1, m2;
     
-    vector<string> arr1, arr2, arr3;
+    for(auto &a : str1) a = tolower(a);
+    for(auto &a : str2) a = tolower(a);
     
-    for(int i = 0;i<str1.length() - 1;i++)
-    {
-        if((str1[i] >= 'a' && str1[i] <= 'z') && (str1[i + 1] >= 'a' && str1[i + 1] <= 'z'))
-        {
-            arr1.push_back(str1.substr(i, 2));
+    for(int i = 0;i < str1.size() - 1;i++){
+        if(isalpha(str1[i]) && isalpha(str1[i + 1])){
+            s1++;
+            m1[str1.substr(i, 2)]++;
         }
     }
     
-    for(int i = 0;i<str2.length() - 1;i++)
-    {
-        if((str2[i] >= 'a' && str2[i] <= 'z') && (str2[i + 1] >= 'a' && str2[i + 1] <= 'z'))
-        {
-            arr2.push_back(str2.substr(i, 2));
+    for(int i = 0;i < str2.size() - 1;i++){
+        if(isalpha(str2[i]) && isalpha(str2[i + 1])){
+            s2++;
+            m2[str2.substr(i, 2)]++;
         }
     }
     
-    int arr1_size = arr1.size();
-    int arr2_size = arr2.size();
+    for(auto [k, v] : m1)
+        cnt += min(v, m2[k]);
     
-    if(arr1_size == 0 && arr2_size == 0)
-        return 65536;
+    answer = (!m1.size() && !m2.size() ? 1 : cnt / (double)(s1 + s2 - cnt));
     
-    for(int i = 0;i<arr1.size();i++)
-    {
-        for(int j = 0;j<arr2.size();j++)
-        {
-            if(arr1[i] == arr2[j])
-            {
-                arr3.push_back(arr1[i]);
-                arr1.erase(arr1.begin() + i);
-                arr2.erase(arr2.begin() + j);
-                i--;
-                j--;
-            }
-        }
-    }
-    return answer = 65536 * arr3.size() / (arr1_size + arr2_size - arr3.size());
+    return int(answer * 65536);
 }

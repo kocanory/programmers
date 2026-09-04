a93a625ab7f13fe51b4c5c8620ad70b31fa0fb74
kocanory <kocan9803@naver.com>
2026-09-04 12:40:52 +0900

    땅따먹기
    - 두 번째 행부터 순회하면서, 열마다 이전 행의 열 값들을 비교해 가장 큰 값을 찾은 뒤 더한다. 이때 같은 열끼리 탐색하지 않는다.
    - 모든 행을 탐색했다면 마지막 행의 열 값들 중 가장 큰 값을 반환한다.

--- level2/땅따먹기.cpp
+++ level2/땅따먹기.cpp
@@ -1,34 +1,23 @@
-#include <iostream>
 #include <vector>
+
 using namespace std;
 
-int dp[100000][4] = {0, };
 int solution(vector<vector<int> > land)
 {
     int answer = 0;
-    
-    for(int i = 0;i<land.size();i++)
-    {
-        for(int j = 0; j < 4;j++)
-        {
-            if(i == 0)
-                dp[i][j] = land[i][j];
-            else
-            {
-                int max = 0;
-                for(int k = 0;k<4;k++)
-                {
-                    if(j != k && dp[i - 1][k] > max)
-                        max = dp[i - 1][k];
-                }
-                dp[i][j] = land[i][j] + max;
+
+    for(int i = 1;i < land.size();i++){
+        for(int j = 0;j < 4;j++){
+            int val = 0;
+            for(int k = 0;k < 4;k++){
+                if(j == k) continue;
+                val = max(val, land[i - 1][k]);
             }
+            land[i][j] += val;
         }
     }
+
     for(int i = 0;i < 4;i++)
-    {
-        if(answer < dp[land.size() - 1][i])
-            answer = dp[land.size() - 1][i];
-    }
+        answer = max(answer, land.back()[i]);
     return answer;
 }

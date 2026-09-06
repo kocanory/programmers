3f58f47d362e5a2a9288ab89c66380c94a1b1809
kocanory <kocan9803@naver.com>
2026-09-06 15:25:49 +0900

    주식가격
    - 각 가격을 순회하면서 arr 벡터 마지막에 저장된 위치에 해당하는 가격과 비교했을 때 현재 가격이 더 작다면 현재 위치에서 arr 벡터 마지막에 저장된 위치를 뺀 값을 answer 벡터에서 해당 위치에 저장한다.
    - 모든 가격을 순회했다면 arr 벡터에 저장된 값에 대하여 마지막 위치에서 arr 벡터에 저장된 값을 뺀 값을 answer 벡터에서 해당하는 위치에 저장한다.
    - 모든 탐색이 끝나면 answer 벡터를 반환한다.

--- level2/주식가격.cpp
+++ level2/주식가격.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
-    for(int i = 0;i<prices.size();i++)
-    {
-        int count = 0;
-        for(int j = i + 1;j<prices.size();j++)
-        {
-            count++;
-            if(prices[i] > prices[j]) break;
+    vector<int> answer(prices.size()), arr;
+    
+    for(int i = 0;i < prices.size();i++){
+        while(!arr.empty() && prices[arr.back()] > prices[i]){
+            answer[arr.back()] = i - arr.back();
+            arr.pop_back();
         }
-        answer.push_back(count);
+        arr.push_back(i);
     }
+    
+    while(!arr.empty()){
+            answer[arr.back()] = prices.size() - arr.back() - 1;
+            arr.pop_back();
+        }
+    
     return answer;
 }

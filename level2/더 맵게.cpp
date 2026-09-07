1c315dca8a37e6bb548ef67583246d576e5b7557
kocanory <kocan9803@naver.com>
2026-09-07 13:44:36 +0900

    더 맵게
    - 스코빌 지수를 우선순위 큐에 넣어 오름차순으로 정렬한다.
    - 가장 작은 스코빌 지수와 그 다음으로 작은 스코빌 지수를 꺼내고 (가장 작은 스코빌 지수 + 그 다음으로 작은 스코빌 지수 * 2)를 우선순위 큐에 넣는다. 그 후 카운트를 증가시킨다.
    - 가장 작은 스코빌지수가 K이상이라면 카운트 answer를 반환한다.
    - 만약 우선 순위 큐의 사이즈가 1 이하라면 더 이상 섞지 못하므로 -1을 반환한다.

--- level2/더 맵게.cpp
+++ level2/더 맵게.cpp
@@ -1,27 +1,22 @@
-#include <string>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    priority_queue <int, vector<int>, greater<int>> pq;
-    for(auto a : scoville)
-        pq.push(a);
+    priority_queue<int> pq;
     
-    while(pq.top() < K && pq.size() > 1)
-    {
-        int first = pq.top();
-        pq.pop();
+    for(auto s : scoville) pq.push(-s); 
+    
+    while(true){
+        if(-pq.top() >= K) return answer;
+        if(pq.size() <= 1) return -1;
         
-        int second = pq.top();
-        pq.pop();
-        
-        int mix = first + second * 2;
-        pq.push(mix);
+        int f1 = -pq.top(); pq.pop();
+        int f2 = -pq.top() * 2; pq.pop();
+        pq.push(-(f1 + f2));
         answer++;
     }
-    if(pq.top() < K) return -1;
-    return answer;
+    
 }

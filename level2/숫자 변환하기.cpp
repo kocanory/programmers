ae239d6685287f375fc5c3aba333e31adc02c92c
kocanory <kocan9803@naver.com>
2026-09-10 16:26:00 +0900

    숫자 변환하기
    - dp, dfs
    - 재귀 함수를 활용해서 더하기, 2 곱하기, 3 곱하기 상황을 모두 구한다. 이때 배열에 방문 카운트를 저장하여 이미 방문한 수 경우 반복하여 방문하지 않도록 한다.
    - 만약 방문 카운트가 갱신되지 않았다면 -1을, 그렇지 않으면 방문 카운트를 반환한다.

--- level2/숫자 변환하기.cpp
+++ level2/숫자 변환하기.cpp
@@ -3,29 +3,25 @@
 
 using namespace std;
 
-int MAX = 1000001;
-int array[1000001] = {0, };
+vector<int> check(1000001);
+int Max = (int)1e9;
 
-int convert(int x, int y, int n, int count)
-{
-    if(array[x] != 0 && array[x] <= count)
-        return MAX;
-    array[x] = count;
-    if(x == y) return count;
-    int plus = MAX, three = MAX, two = MAX;
-    if(x + n <= y)
-        plus = convert(x + n, y, n, count + 1);
-    if(x * 2 <= y)
-        two = convert(x * 2, y, n, count + 1);
-    if(x * 3 <= y)
-        three = convert(x * 3, y, n, count + 1);
-    return min(plus, min(two, three));
+int dfs(int x, int y, int n){
+    if(x == y) return 0;
+    if(check[x]) return check[x];
+    
+    int plus = Max, two = Max, three = Max;
+    if(x + n <= y) plus = dfs(x + n, y, n);
+    if(x * 2 <= y) two = dfs(x * 2, y, n);
+    if(x * 3 <= y) three = dfs(x * 3, y,  n);
+    
+    int res = min(plus, min(two, three));
+    if(res == Max) check[x] = Max;
+    else check[x] = res + 1;
+    return check[x];
 }
 
-
 int solution(int x, int y, int n) {
-    int answer = convert(x, y, n, 0);
-    if(answer >= MAX)
-        answer = -1;
-    return answer;
+    int answer = dfs(x, y, n);
+    return (answer == Max ? -1 : answer);
 }

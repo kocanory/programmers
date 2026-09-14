8e206fbee0f6db8ec1ec9291802893d9410beeee
kocanory <kocan9803@naver.com>
2026-09-14 12:46:39 +0900

    2 x n 타일링
    - dp
    - 타일 1개, 2개일 때 경우의 수를 저장한 다음 3개부터는 뒤에 1개, 2개를 이어붙이는 경우의 수를 더하고 나머지를 취한다.
    - 1개를 세로로 붙이는 경우와 2개를 세로로 붙이는 경우는 겹치므로 이 중 하나는 카운트하지 않는다.
    - 모든 탐색이 끝나면 타일이 n개일 때 경우의 수인 dp[n]을 반환한다.

--- level2/2 x n 타일링.cpp
+++ level2/2 x n 타일링.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 
 using namespace std;
-int array[60001] = {0, };
+
 int solution(int n) {
-    int answer = 0;
-    array[1] = 1;
-    array[2] = 2;
-    for(int i = 3;i<=n;i++)
-        array[i] = (array[i - 1] + array[i - 2]) % 1000000007;
-    return answer = array[n] % 1000000007;
+    vector<long long> dp(n + 1);
+    dp[1] = 1, dp[2] = 2;
+    
+    for(int i = 3;i <= n;i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    
+    return dp[n];
 }

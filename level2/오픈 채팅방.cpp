c7e501c0223ccfe6e8decc2760de604d62765769
kocanory <kocan9803@naver.com>
2026-09-16 15:00:58 +0900

    오픈 채팅방
    - 명령어가 Enter이거나 Change 인 경우 해당 UID에 해당 닉네임을 저장한다.
    - 다시 기록을 순회하면서 Enter이면 해당 UID에 저장된 닉네임이 들어왔다는 메세지를 저장하고, Leave이면 해당 UID에 저장된 닉네임이 나갔다는 메세지를 저장한다.
    - 모든 탐색이 끝나면 메세지가 저장된 answer 벡터를 반환한다.

--- level2/오픈 채팅방.cpp
+++ level2/오픈 채팅방.cpp
@@ -7,33 +7,25 @@ using namespace std;
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
-    vector<pair<string, string>> uid;
-    map<string, string> m;
-    for(auto a : record)
-    {
-        vector<string> rec;
-        stringstream ss(a);
-        string s = "";
-        while(getline(ss, s, ' '))
-            rec.push_back(s);
+    map<string, string> name;
+    
+    for(auto r : record){
+        vector<string> tmp;
+        stringstream ss(r);
+        string s;
+        while(ss >> s) tmp.push_back(s);
         
-        if(rec[0] == "Enter" || rec[0] == "Leave")
-        {
-            uid.push_back(make_pair(rec[0], rec[1]));
-        }
-        if(rec[0] == "Enter" || rec[0] == "Change")
-        {
-            m[rec[1]] = rec[2];
-        }
+        if(tmp[0] == "Enter" || tmp[0] == "Change") name[tmp[1]] = tmp[2];
     }
-    for(auto a : uid)
-    {
-        string str = m[a.second] + "님이 ";
-        if(a.first == "Enter")
-            str += "들어왔습니다.";
-        else
-            str += "나갔습니다.";
-        answer.push_back(str);
+    
+    for(auto r : record){
+        vector<string> tmp;
+        stringstream ss(r);
+        string s;
+        while(ss >> s) tmp.push_back(s);
+        
+        if(tmp[0] == "Enter") answer.push_back(name[tmp[1]] + "님이 들어왔습니다.");
+        else if(tmp[0] == "Leave") answer.push_back(name[tmp[1]] + "님이 나갔습니다.");
     }
     return answer;
 }

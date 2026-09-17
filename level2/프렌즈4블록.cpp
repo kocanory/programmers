ae78042f273827005ca2ba6e84929d6d02a309cd
kocanory <kocan9803@naver.com>
2026-09-17 15:11:36 +0900

    프렌즈4블록
     - 각 보드 위치를 순회하면서 인근 4칸의 모양이 같은지 확인한다. 같다면  해당 위치를 체크한다.
    - 체크한 위치를 순회하면서 보드 칸을 '0'으로 만들고 카운트를 증가시킨다.
    - 열 별로 순회하면서 '0'이 아닌 칸을 찾고 저장한다. 그 후 앞에 '0'을 붙이고, 뒤에 앞서 저장한 칸을 붙인다. 그 후 해당 칸을 보드에 저장한다.
    - 만약 보드를 탐색하면서 같은 4블록이 없다면 탐색을 종료한다.
    - 모든 탐색이 끝나면 카운트 answer를 반환한다.

--- level2/프렌즈4블록.cpp
+++ level2/프렌즈4블록.cpp
@@ -5,51 +5,45 @@ using namespace std;
 
 int solution(int m, int n, vector<string> board) {
     int answer = 0;
-    vector<string> back;
-    while(true)
-    {
+    
+    while(true){
         bool flag = false;
-        back = board;
-        for(int i = 0;i < m - 1;i++)
-        {
-            for(int j = 0;j < n - 1;j++)
-            {
-                if(back[i][j] != '0' && back[i][j] == back[i][j + 1] && back[i][j] == back[i + 1][j] && back[i][j] == back[i + 1][j + 1])
-                {
-                    if(!flag) flag = true;
-                    board[i][j] = '0';
-                    board[i][j + 1] = '0';
-                    board[i + 1][j] = '0';
-                    board[i + 1][j + 1] = '0';
-                }                    
-            }
-        }
-        if(!flag) break;
-        for(int i = m - 1;i>=0;i--)
-        {
-            for(int j = 0;j<n;j++)
-            {
-                if(board[i][j] == '0')
-                {
-                    for(int k = i - 1;k>=0;k--)
-                    {
-                        if(board[k][j] != '0')
-                        {
-                            char temp = board[k][j];
-                            board[k][j] = board[i][j];
-                            board[i][j] = temp;
-                            break;
-                        }
-                    }
+        vector<vector<bool>> check(m, vector(n, false));
+        
+        for(int i = 0;i < m - 1;i++){
+            for(int j = 0;j < n - 1;j++){
+                if(board[i][j] == '0') continue;
+                if(board[i][j] == board[i][j + 1] && board[i][j] == board[i + 1][j] && board[i][j] == board[i + 1][j + 1]){
+                    flag = true;
+                    check[i][j] = true;
+                    check[i][j + 1] = true;
+                    check[i + 1][j] = true;
+                    check[i + 1][j + 1] = true;
                 }
             }
         }
+        
+        if(!flag) break;
+        
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(check[i][j]){
+                    answer++;
+                    board[i][j] = '0';
+                }
+            }
+        }
+        
+        for(int j = 0;j < n;j++){
+            string tmp = "";
+            for(int i = 0;i < m;i++)
+                if(board[i][j] != '0') tmp += board[i][j];
+            tmp = string(m - tmp.size(), '0') + tmp;
+            
+            for(int i = 0;i < m;i++)
+                board[i][j] = tmp[i];
+        }
     }
-    for(int i = 0;i<m;i++)
-    {
-        for(int j = 0;j<n;j++)
-            if(board[i][j] == '0')
-                answer++;
-    }
+    
     return answer;
 }

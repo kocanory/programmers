5f01699363da9a4a0a6a49ca27c23b7a710d3e61
kocanory <kocan9803@naver.com>
2026-09-19 14:53:18 +0900

    2개 이하로 다른 비트
    - 만약 현재 수가 짝수라면 일의 자리 비트가 0이므로 해당 비트만 1로 바꾸면 된다. 따라서 1을 더한 수를 벡터에 저장한다.
    - 만약 현재 수가 홀수라면 각 비트를 순회하면서 0인 자리를 찾는다. 찾았다면 해당 비트를 1로 만들기 위해 해당 비트에 해당하는 수를 더한다. 그 후 해당 비트 오른쪽에 있는 수를 빼서 가장 작은 수로 만든다. 그 후 수를 벡터에 저장한다.
    - 모든 탐색이 끝나면 수가 저장된 벡터 answer를 반환한다.

--- level2/2개 이하로 다른 비트.cpp
+++ level2/2개 이하로 다른 비트.cpp
@@ -1,26 +1,22 @@
 #include <string>
 #include <vector>
-#include <cmath>
+
 using namespace std;
 
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
-    for(auto a : numbers)
-    {
-        if(a % 2 == 0)
-            answer.push_back(a + 1);
-        else
-        {
-            long long num = a, mul = 1;
-            while(num > 0)
-            {
-                if((num & 1) == 0)
-                    break;
-                mul *= 2;
-                num >>= 1;
+    
+    for(auto n : numbers){
+        if(!n % 2) answer.push_back(n + 1);
+        else{
+            long long val = 1;
+            while(n > val){
+                if((n & val) == 0) break;
+                val <<= 1;
             }
-            answer.push_back(a + mul - mul / 2);
+            answer.push_back(n + val - val / 2);
         }
     }
+    
     return answer;
 }

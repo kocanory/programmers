901d3dd21816b4070988ae2d61821488667ed028
kocanory <kocan9803@naver.com>
2026-09-22 13:29:20 +0900

    쿼드 압축 후 개수 세기
    - 재귀함수를 통해 범위를 줄여가면서 해당 범위 안에 있는 값들이 모두 같은지 확인한다. 모두 같다면 값의 카운트를 증가시키고 해당 범위를 탐색하지 않는다. 값이 다르다면 범위를 4등분하여 각각의 범위에 대해 탐색을 진행한다.
    - 모든 탐색이 끝나면 0의 개수와 1의 개수를 반환한다.

--- level2/쿼드 압축 후 개수 세기.cpp
+++ level2/쿼드 압축 후 개수 세기.cpp
@@ -5,37 +5,32 @@ using namespace std;
 
 int zero = 0, one = 0;
 
-bool check(int x, int y, int size, vector<vector<int>> &arr)
-{
+bool check(int x, int y, int size, vector<vector<int>> &arr){
     int val = arr[x][y];
-    for(int i = x; i < x + size;i++)
-        for(int j = y; j < y + size;j++)
-            if(arr[i][j] != val)
+    
+    for(int i = x;i < x + size;i++)
+        for(int j = y;j < y + size;j++)
+            if(val != arr[i][j])
                 return false;
+    
     return true;
 }
 
-void dfs(int x, int y, int size, vector<vector<int>> &arr)
-{
-    if(check(x, y, size, arr))
-    {
-        if(arr[x][y] == 0)
-            zero++;
-        else
-            one++;
+void dfs(int x, int y, int size, vector<vector<int>> &arr){
+    if(check(x, y, size, arr)){
+        if(arr[x][y]) one++;
+        else zero++;
         return;
     }
+    
     size /= 2;
     dfs(x, y, size, arr);
-    dfs(x, y + size, size, arr);
     dfs(x + size, y, size, arr);
+    dfs(x, y + size, size, arr);
     dfs(x + size, y + size, size, arr);
 }
 
 vector<int> solution(vector<vector<int>> arr) {
-    vector<int> answer;
     dfs(0, 0, arr.size(), arr);
-    answer.push_back(zero);
-    answer.push_back(one);
-    return answer;
+    return {zero, one};
 }
